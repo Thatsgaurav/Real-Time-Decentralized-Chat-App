eee075391d649a605c06d98fccf3e8062f82d85c
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2022-10-12 21:09:06 +0530

    commit

--- client/components/ChatHeader.js
+++ client/components/ChatHeader.js
@@ -1,28 +1,29 @@
-import Image from "next/image";
-import personPlus from "../assets/icons/person-plus.svg";
-import video from "../assets/icons/video.svg";
-import inbox from "../assets/icons/inbox.svg";
-import phone from "../assets/icons/phone.svg";
-import help from "../assets/icons/help.svg";
-import pin from "../assets/icons/pin.svg";
-import at from "../assets/icons/at.svg";
-import styles from "../styles/chatHeader.module.css";
-import ethLogo from "../assets/eth.png";
-
-const currentAccount = "0x8c3d";
+import Image from 'next/image'
+import personPlus from '../assets/icons/person-plus.svg'
+import video from '../assets/icons/video.svg'
+import inbox from '../assets/icons/inbox.svg'
+import phone from '../assets/icons/phone.svg'
+import help from '../assets/icons/help.svg'
+import pin from '../assets/icons/pin.svg'
+import at from '../assets/icons/at.svg'
+import styles from '../styles/chatHeader.module.css'
+import ethLogo from '../assets/eth.png'
+import { useContext } from 'react'
+import { DiscordContext } from '../context/context'
 
 const ChatHeader = () => {
+  const { roomName, currentAccount, connectWallet } = useContext(DiscordContext)
   return (
-    <div className={styles.ChatHeader}>
+    <div className={styles.chatHeader}>
       <div className={styles.roomNameContainer}>
-        <Image height={20} width={20} src={at} className={styles.svg} alt="" />
-        <h3 className={styles.title}>Room Name</h3>
-        <div className={styles.chatHeaderStatus} id="online" />
+        <Image height={20} width={20} src={at} className={styles.svg} alt='' />
+        <h3 className={styles.title}>{roomName}</h3>
+        <div className={styles.chatHeaderStatus} id='online' />
       </div>
       {currentAccount ? (
-        <div className={styles.connectWallet}>
-          <Image src={ethLogo} height={20} width={15} alt="ethLogo" />
-          <span className={styles.separator}>{"|"}</span>
+        <div className={styles.connectedWallet}>
+          <Image src={ethLogo} height={20} width={15} alt='ethLogo' />
+          <span className={styles.separator}>{'|'}</span>
           {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
         </div>
       ) : (
@@ -38,7 +39,7 @@ const ChatHeader = () => {
             width={25}
             src={phone}
             className={styles.svg}
-            alt=""
+            alt=''
           />
         </div>
         <div className={styles.headerItem}>
@@ -47,15 +48,45 @@ const ChatHeader = () => {
             width={25}
             src={video}
             className={styles.svg}
-            alt=""
+            alt=''
           />
         </div>
       </div>
       <div className={styles.headerItem}>
-        <Image height={25} width={25} src={pin} className={styles.svg} alt="" />
+        <Image height={25} width={25} src={pin} className={styles.svg} alt='' />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={personPlus}
+          className={styles.svg}
+          alt=''
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <input type='search' placeholder='Search' />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={inbox}
+          className={styles.svg}
+          alt=''
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={help}
+          className={styles.svg}
+          alt=''
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ChatHeader;
+export default ChatHeader
--- client/components/ConversationList.js
+++ client/components/ConversationList.js
@@ -1,43 +1,26 @@
-import styles from "../styles/conversationList.module.css";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import friends from "../assets/icons/friends.svg";
-import nitro from "../assets/icons/nitro.svg";
-import avatar1 from '../assets/avatar-1.webp'
-import avatar2 from '../assets/avatar-2.png'
-import avatar3 from '../assets/avatar-3.webp'
-import avatar4 from '../assets/avatar-4.webp'
+import styles from '../styles/conversationList.module.css'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import friends from '../assets/icons/friends.svg'
+import nitro from '../assets/icons/nitro.svg'
 import DmCard from './DmCard'
 
-const dummyDms = [
-  {
-      id: 1,
-      name: 'Harry',
-      avatar: avatar1,
-  },
-  {
-      id: 2,
-      name: 'Tripy',
-      avatar: avatar2,
-  },
-  {
-      id: 3,
-      name: 'Bagha',
-      avatar: avatar3,
-  },
-  {
-      id: 4,
-      name: 'Karan',
-      avatar: avatar4,
-  },
-]
-
 const ConversationList = () => {
-  const [dms, setDms] = useState(dummyDms)
+  const [dms, setDms] = useState([])
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getdms`)
+
+      setDms(await response.json())
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
   return (
     <div className={styles.conversations}>
-      <div className={styles.ConversationListTop}>
-        <input type="search" placeholder="Find or start a conversation" />
+      <div className={styles.conversationListTop}>
+        <input type='search' placeholder='Find or start a conversation' />
       </div>
       <div className={styles.conversationsContainer}>
         <div className={styles.elementsContainer}>
@@ -47,7 +30,7 @@ const ConversationList = () => {
               width={25}
               src={friends}
               className={styles.svg}
-              alt="friends"
+              alt='friends'
             />
           </div>
           <p>Fiends</p>
@@ -59,7 +42,7 @@ const ConversationList = () => {
               width={25}
               src={nitro}
               className={styles.svg}
-              alt="nitro"
+              alt='nitro'
             />
           </div>
           <p>Nitro</p>
@@ -67,19 +50,19 @@ const ConversationList = () => {
         <div className={styles.dmTitle}>DIRECT MESSAGES</div>
         {dms.map((dm, index) => (
           <DmCard
-          key={index}
-          name={dm.name}
-          id={dm.id}
-          avatar={
-            dm.avatar ||
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU'
-          }
-          status='online'
-        />
+            key={index}
+            name={dm.name}
+            id={dm.id}
+            avatar={
+              dm.avatar ||
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU'
+            }
+            status='online'
+          />
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ConversationList;
+export default ConversationList
